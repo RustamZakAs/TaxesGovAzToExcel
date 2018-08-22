0a82cb1f0faae0312e2b195bc6ea659febb154b4
RustamZakAs <rostik055@gmail.com>
2018-08-22 18:31:56 +0400

    changes

--- TaxesGovAzToExcel/Main.Designer.cs
+++ TaxesGovAzToExcel/Main.Designer.cs
@@ -28,20 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.labelNov = new System.Windows.Forms.Label();
             this.comboBoxNov = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxHereket = new System.Windows.Forms.ComboBox();
+            this.labelHereket = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // labelNov
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Məlumat növünü seçin:";
+            this.labelNov.AutoSize = true;
+            this.labelNov.Location = new System.Drawing.Point(12, 9);
+            this.labelNov.Name = "labelNov";
+            this.labelNov.Size = new System.Drawing.Size(117, 13);
+            this.labelNov.TabIndex = 0;
+            this.labelNov.Text = "Məlumat növünü seçin:";
             // 
             // comboBoxNov
             // 
@@ -50,36 +52,49 @@
             this.comboBoxNov.Name = "comboBoxNov";
             this.comboBoxNov.Size = new System.Drawing.Size(345, 21);
             this.comboBoxNov.TabIndex = 1;
+            this.comboBoxNov.SelectedIndexChanged += new System.EventHandler(this.comboBoxNov_Leave);
+            this.comboBoxNov.Leave += new System.EventHandler(this.comboBoxNov_Leave);
             // 
-            // comboBox2
+            // comboBoxHereket
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(15, 72);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(345, 21);
-            this.comboBox2.TabIndex = 2;
+            this.comboBoxHereket.FormattingEnabled = true;
+            this.comboBoxHereket.Location = new System.Drawing.Point(15, 72);
+            this.comboBoxHereket.Name = "comboBoxHereket";
+            this.comboBoxHereket.Size = new System.Drawing.Size(345, 21);
+            this.comboBoxHereket.TabIndex = 2;
             // 
-            // label2
+            // labelHereket
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 56);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(115, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Hərəkət növünü seçin:";
+            this.labelHereket.AutoSize = true;
+            this.labelHereket.Location = new System.Drawing.Point(12, 56);
+            this.labelHereket.Name = "labelHereket";
+            this.labelHereket.Size = new System.Drawing.Size(115, 13);
+            this.labelHereket.TabIndex = 3;
+            this.labelHereket.Text = "Hərəkət növünü seçin:";
             // 
-            // Form1
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TaxesGovAzToExcel.Properties.Resources.question_mark_PNG48;
+            this.pictureBox1.Location = new System.Drawing.Point(558, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(28, 26);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(375, 304);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox2);
+            this.ClientSize = new System.Drawing.Size(588, 304);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelHereket);
+            this.Controls.Add(this.comboBoxHereket);
             this.Controls.Add(this.comboBoxNov);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Controls.Add(this.labelNov);
+            this.Name = "Main";
+            this.Text = "AR İnternet Vergi İdarəsinin elektron saytından məlumatların alınması";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -87,10 +102,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelNov;
         private System.Windows.Forms.ComboBox comboBoxNov;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxHereket;
+        private System.Windows.Forms.Label labelHereket;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- TaxesGovAzToExcel/Main.cs
+++ TaxesGovAzToExcel/Main.cs
@@ -13,17 +13,27 @@ namespace TaxesGovAzToExcel
         public Main()
         {
             InitializeComponent();
+            //Text = "Azərbaycan Respublikasının İnternet Vergi İdarəsinin elektron saytından məlumatların alınması";
+            Text = "AR İnternet Vergi İdarəsinin elektron saytından məlumatların alınması";
+
+            /*0*/comboBoxNov.Items.Add("Elektron Vergi Hesab Faktralar üzrə məlumatın alınması");
+            /*1*/comboBoxNov.Items.Add("Elektro Qaimələr üzrə məlumatın alınması");
+            /*2*/comboBoxNov.Items.Add("Əvəzləşmə ayı üzrə məlumatın alınması");
+            /*3*/comboBoxNov.Items.Add("Depozit hesabından hərəkətin çıxarışı");
+
+            if (comboBoxNov.Items.Count > 0) comboBoxNov.SelectedIndex = 0;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void comboBoxNov_Leave(object sender, EventArgs e)
         {
-            comboBoxNov.Items.Add("Elektron Vergi Hesab Faktralar üzrə məlumatın alınması");
-            comboBoxNov.Items.Add("Elektro Qaimələr üzrə məlumatın alınması");
-            comboBoxNov.Items.Add("Əvəzləşmə ayı üzrə məlumatın alınması");
-            comboBoxNov.Items.Add("Depozit hesabından hərəkətin çıxarışı");
-            
-
-
+            comboBoxHereket.Items.Clear();
+            if (comboBoxNov.SelectedIndex == 0 || comboBoxNov.SelectedIndex == 1)
+            {
+                comboBoxHereket.Items.Add("Gələnlər");
+                comboBoxHereket.Items.Add("Göndərdiklərim");
+            }
+            comboBoxHereket.Text = "";
+            if (comboBoxHereket.Items.Count > 0) comboBoxHereket.SelectedIndex = 0;
         }
     }
 }
